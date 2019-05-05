eb65b0077a27b48c1a0fe337d1137047d50d5b35
smahot <steve.mahot@edu.devinci.fr>
2019-05-05 21:13:56 +0200

    add test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+#Fichier des tests de fonctions
+from gomoku import gomoku
+
+g = gomoku()
+
+# Test ligne5suite # OK
+#==================#
+liste1 = [' ', ' ', 'B', 'N', 'N', 'B', 'N', 'N', 'B', ' ', 'N', 'N', 'N', 'N', 'N'] #droite5
+liste2 = ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'B', ' ', 'N', 'N', 'B', 'N', 'N'] #gauche5+
+liste3 = [' ', ' ', 'B', 'N', 'N', 'N', 'N', 'N', 'B', ' ', 'N', 'B', 'N', 'N', 'N'] #centre5
+liste4 = [' ', 'B', 'B', 'B', 'B', 'B', 'N', 'N', 'B', ' ', 'N', 'N', 'N', 'N', 'N'] #double5BN
+liste5 = [' ', ' ', 'N', ' ', ' ', 'B', 'B', 'B', 'B', ' ', 'N', 'B', ' ', ' ', 'N'] #centre4
+liste6 = [' ', ' ', ' ', ' ', ' ', 'B', 'N', 'N', 'B', ' ', 'N', 'B', ' ', ' ', 'N'] #vide5
+
+print(g.ligne5suite(liste1))
+print(g.ligne5suite(liste2))
+print(g.ligne5suite(liste3))
+print(g.ligne5suite(liste4))
+print(g.ligne5suite(liste5))
+print(g.ligne5suite(liste6))
+
+# Résultat N N N B False False
+# Test Réussi
