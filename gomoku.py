2e16138d6339434cf5873d76a23d2bbabc649397
smahot <steve.mahot@edu.devinci.fr>
2019-05-05 20:41:58 +0200

    start from morpion-minimax

---
+++ gomoku.py
@@ -0,0 +1,154 @@
+# Application de l'algorithme minimax au gomoku
+# Fait par Steve MAHOT et Matthieu LOUF
+
+class gomoku:
+
+    def __init__(self):
+        self.grille = [[' ' for j in range(3)] for i in range(3)]
+        self.tour = "X"
+
+    def display(self):
+        print('    0   1   2')
+        print("  -------------")
+        for i in range(3):
+            ligne= str(i)+" |"
+            for j in range(3):
+                ligne+=' '+self.grille[i][j]+ ' |'
+            print(ligne,"\n  -------------")
+
+    def tourSuivant(self):
+        if self.tour == "X":
+            self.tour = "O"
+        else :
+            self.tour = "X"
+    
+    def gagnant(self):
+        res = False
+        #3 lignes :
+        for i in range (3):
+            if res == False and self.identiqueList(self.grille[i]):
+                res = self.grille[i][0]
+
+        #3 colonnes :
+        if res == False:
+            for i in range (3):
+                if res == False and self.identiqueList(self.grille[j][i] for j in range(3)):
+                    res = self.grille[0][i]
+
+        #2 diagonales :
+        if res == False and self.identiqueList(self.grille[i][i] for i in range(3)):
+            res = self.grille[0][0]
+        if res == False and self.identiqueList(self.grille[i][2-i] for i in range(3)):
+            res = self.grille[0][2]
+        return res
+
+    def matchNul (self):
+        res = True
+        if self.gagnant() == "X" or self.gagnant() == "O":
+            res = False
+        for i in range(3):
+            if " " in self.grille[i]:
+                res = False
+        return res
+        
+    def identiqueList(self, liste):
+        liste = list(liste)
+        res = True
+
+        if liste[0] == ' ': #exception
+            return False
+
+        for i in liste:
+            if i != liste[0]:
+                res = False
+        return res
+
+    def utility (self):
+        if self.matchNul():
+            return 0
+        if self.gagnant() == self.tour:
+            return 1
+        else:
+            return -1
+    
+    def Actions(self):
+        acts = []
+        for i in range(3):
+            for j in range(3):
+                if(self.grille[i][j]==' '):
+                    acts.append([i,j])
+        return acts
+
+    def Results(self,position,joueur):
+        self.grille[position[0]][position[1]]=joueur
+
+    def MinMax(self):
+        actions = list()
+        actions.append(self.Actions())
+        actions.append(list())
+        for i in range(len(actions[0])):
+            self.grille[actions[0][i][0]][actions[0][i][1]] = self.tour
+            min_utility = self.Min()
+            self.grille[actions[0][i][0]][actions[0][i][1]] = " "
+            actions[1].append(min_utility)
+
+        index = actions[1].index(max(actions[1]))
+        best = list()
+        best.append(actions[0][index])
+        best.append(actions[1][index])
+
+        print("Actions possibles :",actions[0])
+        print("Scores correspondants :",actions[1])
+        return best
+    
+    def Max(self):
+        if self.gagnant() != False or self.matchNul():
+            return self.utility()
+
+        actions = list()
+        actions.append(self.Actions())
+        actions.append(list())
+        joueur = self.tour
+        if len(actions[0]) == 1:
+            self.grille[actions[0][0][0]][actions[0][0][1]] = joueur
+            utility = self.utility()
+            self.grille[actions[0][0][0]][actions[0][0][1]] = " "
+            return utility
+        
+        for i in range(len(actions[0])):
+            self.grille[actions[0][i][0]][actions[0][i][1]] = joueur
+            min_utility = self.Min()
+            self.grille[actions[0][i][0]][actions[0][i][1]] = " "
+            actions[1].append(min_utility)
+        
+        max_utility = max(actions[1])
+        return max_utility
+
+
+    def Min(self):
+        if self.gagnant() != False or self.matchNul():
+            return self.utility()
+
+        actions = list()
+        actions.append(self.Actions())
+        actions.append(list())
+        
+        if self.tour == 'X':
+            joueur = 'O'
+        else :
+            joueur = 'X'
+
+        if len(actions[0]) == 1:
+            self.grille[actions[0][0][0]][actions[0][0][1]] = joueur
+            utility = self.utility()
+            self.grille[actions[0][0][0]][actions[0][0][1]] = " "
+            return utility
+        
+        for i in range(len(actions[0])):
+            self.grille[actions[0][i][0]][actions[0][i][1]] = joueur
+            max_utility = self.Max()
+            self.grille[actions[0][i][0]][actions[0][i][1]] = " "
+            actions[1].append(max_utility)
+        
+        min_utility = min(actions[1])
+        return min_utility
---
+++ main.py
@@ -0,0 +1,93 @@
+# Application de l'algorithme minimax au gomoku
+# Fait par Steve MAHOT et Matthieu LOUF
+
+#------------ IMPORTATION MODULES -------------#
+import random as rnd
+import os
+#classe du gomoku dans le fichier gomoku.py
+from gomoku import gomoku
+
+#------------ DEFINITION FONCTIONS -------------#
+
+#fonction pour nettoyer la console
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def humain_vs_humain():
+    m = gomoku()
+
+    print("Début de la partie : \n")
+
+    while not (m.gagnant() or m.matchNul()) :
+        
+        print("Au tour du joueur \'",m.tour,"\' :")
+        m.display()
+
+        print("-> Calcul des actions possibles (peut durer 10s)\n")
+        print("Coordonnée conseillée :",m.MinMax(),"\n")
+        x = int(input("Jouer à la coordonnée x = "))
+        y = int(input("                      y = "))
+
+        m.Results([x,y],m.tour)
+        m.tourSuivant()
+
+        cls()
+        print("\n")
+
+    m.display()
+    gagnant=m.gagnant()
+    if gagnant==False:
+        print("Match nul :/\n")
+    else:
+        print("Le gagnant est le joueur \'",m.gagnant(),"\'! :D\n")
+
+def humain_vs_ia():
+    m = gomoku()
+
+    rnd.seed()
+    type_joueur =['joueur','ia']
+    joueur_actuel = rnd.randint(0,1)
+
+    print("Début de la partie : \n")
+
+    while not (m.gagnant() or m.matchNul()) :
+
+        print("Au tour de ",type_joueur[joueur_actuel]," \'",m.tour,"\' :")
+        m.display()
+
+        if(type_joueur[joueur_actuel]=='joueur'):
+            x = int(input("Jouer à la coordonnée x = "))
+            y = int(input("                      y = "))
+            m.Results([x,y],m.tour)
+        
+        else :
+            print("-> Calcul des actions possibles (peut durer 10s)\n")
+            choix_ia=m.MinMax()
+            m.Results([choix_ia[0][0],choix_ia[0][1]],m.tour)
+        
+        m.tourSuivant()
+        joueur_actuel+=1
+        if joueur_actuel==2:
+            joueur_actuel=0
+
+        cls()
+        print("\n")
+
+    m.display()
+    gagnant=m.gagnant()
+    if gagnant==False:
+        print("Match nul :/\n")
+    else:
+        print("Le gagnant est \'",m.gagnant(),"\'! :D\n")
+
+def menu():
+    print("\n 1 : Humain vs Humain assisté par ordinateur\n 2 : Humain vs IA\n")
+    choix = int(input("Mode de jeu : "))
+
+    if choix==1:
+        humain_vs_humain()
+    if choix==2:
+        humain_vs_ia()
+
+#------------ Lancement du menu -------------#
+menu()
